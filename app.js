563d11889fb9041e11d2bb74ca8e14aafe010fa2
Ronier Lima <ronier.lim@gmail.com>
2023-09-30 09:50:08 -0300

    add bcrypt

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -8,9 +9,21 @@ const prisma = new PrismaClient();
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  const users = await prisma.user.findMany()
-  
+  const users = await prisma.user.findMany();
+
   res.send(users);
+});
+
+app.post("/", async (req, res) => {
+  const user = req.body;
+
+  user.senha = bcrypt.hash(user.senha, 12);
+
+  const newUser = await prisma.user.create({
+    data: user,
+  });
+
+  res.send(newUser);
 });
 
 app.listen(3000, () => {
