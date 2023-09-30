a208f44178012bd1f107613239867ea3fb73c4ec
Ronier Lima <ronier.lim@gmail.com>
2023-09-30 09:14:11 -0300

    add route get users

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const express = require('express');
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 
+const prisma = new PrismaClient();
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  const users = prisma.user.findMany()
+  
+  res.send(users);
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
